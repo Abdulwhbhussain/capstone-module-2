9f8c8b9f8722e2b7f40e822112d72cfc0ef01d79
WHB901 <abdulwhbhussain@gmail.com>
2023-05-23 12:51:41 +0500

    Basic Markup Header Footer and Main section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 import './styles.css';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const h2 = document.createElement('h2');
-  h2.classList.add('color');
-  h2.innerHTML = 'in the index.js file';
+function appRender() {
+    const capstoneContainer = document.getElementsByClassName('capstone-container')[0];
+    console.log(capstoneContainer);
+    capstoneContainer.innerHTML = `<!-- Header of the Api Based Web App -->
+    <header class="header-container">
+      <!-- Logo of the Api Based Web App -->
+      <div class="logo">
+        <p>Logo Here!</p>
+      </div>
+      <!-- Navbar of The Api based web app -->
+      <nav>
+        <ul>
+          <li><a href="#">Spaceships(6)</a></li>
+          <li><a href="#">Planets</a></li>
+          <li><a href="#">Races</a></li>
+        </ul>
+      </nav>
+    </header>
+    <!-- Main Content of the Api Based Web App -->
+    <main>
+      <!-- Section of the Api Based Web App -->
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+      <h5>dummy</h5>
+    </main>
+    <!-- Footer of the Api Based Web App -->
+    <footer class="footer-container">
+      <p>© Created by Microverse under CC license</p>
+    </footer>`;
+}
 
-  document.body.appendChild(h2);
-});
+document.addEventListener('DOMContentLoaded', appRender());
