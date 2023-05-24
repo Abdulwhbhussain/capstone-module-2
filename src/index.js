17a5537d43142024b866353aa2e34f66322f2481
WHB901 <abdulwhbhussain@gmail.com>
2023-05-24 20:53:55 +0500

    Involvement API and comments Functionality achieved

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 
 const appID = 'YOYr3lMRRi289YuVJOhS';
 const urlForLikes = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/likes/`;
-const urlForComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/comments/`;
+const urlForComments = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${appID}/comments`;
 
 function appRender() {
   const capstoneContainer = document.getElementsByClassName('capstone-container')[0];
@@ -59,7 +59,115 @@ const foodByChinaList = async () => {
       food.likes = like.likes;
     }
   });
+
+  // get Comments from the Involvement API
+  const urlForGettingComments = urlForComments + '?item_id=';
+  foodByChina.forEach( async (food) => {
+    const url = urlForGettingComments + food.id;
+    const comments = await fetch(url).then((response) => response.json()).then((data) => data);
+    food.comments = comments;
+  });
   return foodByChina;
+};
+
+// Post Comments Functions 
+const addComment = async (date, id, name, comment) => {
+    const requestToResponse = await fetch(urlForComments, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            item_id: id,
+            username: name,
+            comment: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} ${name}: ${comment}`,
+        }),
+    });
+    const data = await requestToResponse.text();
+    if (data === 'Created') {
+    return data;
+    }
+}
+
+// comments Render
+const commentsObjectRender = (commentsArray) => {
+    const popupComments = document.createElement('div');
+    popupComments.classList.add('popup-comments');
+    const comments = commentsArray.forEach((comment) => {
+        const commentElement = document.createElement('div');
+        commentElement.classList.add('individual-comment');
+        commentElement.innerText = comment.comment;
+        popupComments.appendChild(commentElement);
+    });
+    return popupComments.innerHTML;
+};
+
+// Create Comments popup
+const popupContainer = (foodObject) => {
+    // Background blur
+    const popupContainerBackground = document.createElement('div');
+    popupContainerBackground.classList.add('popup-container-background');
+
+    // popup Food Contents Container
+    const popupContainer = document.createElement('div');
+    popupContainer.classList.add('popup-container');
+    popupContainer.innerHTML = `
+    <!-- Close Times Icon -->
+    <button data-close-button class="close-button">&times;</button>
+    <!-- Food Image -->
+    <div class="popup-image-container">
+        <img class="popup-image" src=${foodObject.image} alt=${foodObject.name} draggable="false" />
+    </div>
+    <!-- Food Name -->
+    <div class="popup-name-container"><h2>${foodObject.name}</h2></div>
+    <!-- Food Details -->
+    <div class="popup-details-container">
+    <div class="popup-details-upper"><span>Count: ${foodObject.count}</span><span>ID: ${foodObject.id}</span></div>
+    <div class="popup-details-lower"><span>Likes: ${foodObject.likes}</span><span>Delicious: Yes</span></div>
+    </div>
+    <!-- Comments and comment Details -->
+    <div class="popup-comments-container">
+    <h2>Comments (${foodObject.comments.length})</h2>
+    <div class="popup-comments">${commentsObjectRender(foodObject.comments)}</div>
+    <!-- Add a Comment -->
+    <div class="add-comment">
+        <h2>Add a Comment</h2>
+        <form class="add-comment-form">
+        <input type="text" name="name" id="name" placeholder="Your Name" />
+        <textArea type="text" name="comment" id="comment" placeholder="Your Insights"></textArea>
+        <button type="submit" class="add-comment-button">Comment</button>
+        </form>
+    </div>
+    </div>
+    `;
+
+    popupContainerBackground.appendChild(popupContainer);
+
+    document.body.appendChild(popupContainerBackground);
+    document.querySelector('.close-button').addEventListener('click', () => {
+        popupContainerBackground.remove();
+    });
+    document.querySelector('.add-comment-form').addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const date = new Date();
+        const name = document.querySelector('#name').value;
+        const comment = document.querySelector('#comment').value;
+        const resp = await addComment(date, foodObject.id, name, comment);
+        if (resp === 'Created') {
+            const commentElement = document.createElement('div');
+            commentElement.classList.add('individual-comment');
+            commentElement.innerText = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} ${name}: ${comment}`;
+            document.querySelector('.popup-comments').appendChild(commentElement);
+            foodObject.comments.push({
+                comment: `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} ${name}: ${comment}`,
+                creation_date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+                username: name,
+            });
+            document.querySelector('.popup-comments-container h2').innerText = `Comments (${foodObject.comments.length})`;
+        };
+        document.querySelector('#name').value = '';
+        document.querySelector('#comment').value = '';
+    });
 };
 
 // Render Food Grid Cards
@@ -104,6 +212,14 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   renderFoodGridCards(foodByChina);
 
+//   createCommentsPopups(foodByChina);
+
+  // Send Comments request to the Involvment Api
+//   const date = new Date();
+//   foodByChina.forEach( async (food) => {
+//     const commentPost = await addComment(date, food.id, 'Enobong', 'testing comments');
+//   });
+
   // Likes Buttons Event Listener
     const likesButtons = document.querySelectorAll('.food-card-heart');
     for (let likesButton of likesButtons) {
@@ -142,6 +258,11 @@ document.addEventListener('DOMContentLoaded', async () => {
     for (let commentsButton of commentsButtons) {
         commentsButton.addEventListener('click', () => {
             console.log('comments button clicked', commentsButton);
+            const foodTitleElement = commentsButton.previousElementSibling.previousElementSibling.previousElementSibling;
+            const foodTitle = foodTitleElement.innerText;
+            const foodArrayIndex = foodByChina.findIndex((food) => food.name === foodTitle);
+            // create popup
+            popupContainer(foodByChina[foodArrayIndex]);
         });
     };
     
