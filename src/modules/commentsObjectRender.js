4bdcbc697a7ce271837ec0611d611213529aab72
WHB901 <abdulwhbhussain@gmail.com>
2023-05-26 03:16:38 +0500

    Major UI/UX Improvements at Comments and Finishing Touches

--- src/modules/commentsObjectRender.js
+++ src/modules/commentsObjectRender.js
@@ -1,11 +1,11 @@
 const commentsObjectRender = (commentsArray) => {
   const popupComments = document.createElement('div');
   popupComments.classList.add('popup-comments');
-  const comments = commentsArray.forEach((comment) => {
-      const commentElement = document.createElement('div');
-      commentElement.classList.add('individual-comment');
-      commentElement.innerText = comment.comment;
-      popupComments.appendChild(commentElement);
+  commentsArray.forEach((comment) => {
+    const commentElement = document.createElement('div');
+    commentElement.classList.add('individual-comment');
+    commentElement.innerText = comment.comment;
+    popupComments.appendChild(commentElement);
   });
   return popupComments.innerHTML;
 };
